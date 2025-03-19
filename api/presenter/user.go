def2062a73d4db1355e7d1237bdd62d420471e43
Konstantin Lapkov <k.lapkov@sap.com>
2025-03-19 13:18:24 +0200

    Mock user endpoints

--- api/presenter/user.go
+++ api/presenter/user.go
@@ -2,17 +2,40 @@ package presenter
 
 import (
 	"net/url"
+	"time"
 
 	"code.cloudfoundry.org/korifi/api/repositories/include"
+	"code.cloudfoundry.org/korifi/tools"
 )
 
 const usersBase = "/v3/users"
 
 type UserResponse struct {
-	Name string `json:"username"`
-	GUID string `json:"guid"`
+	GUID             string    `json:"guid"`
+	CreatedAt        string    `json:"created_at"`
+	UpdatedAt        string    `json:"updated_at"`
+	Name             string    `json:"username"`
+	PresentationName string    `json:"presentation_name"`
+	Origin           string    `json:"origin"`
+	Links            UserLinks `json:"links"`
 }
 
-func ForUser(name string, _ url.URL, includes ...include.Resource) UserResponse {
-	return UserResponse{Name: name, GUID: name}
+type UserLinks struct {
+	Self Link `json:"self"`
+}
+
+func ForUser(name string, baseURL url.URL, includes ...include.Resource) UserResponse {
+	return UserResponse{
+		GUID:             name,
+		CreatedAt:        tools.ZeroIfNil(formatTimestamp(tools.PtrTo(time.Now()))),
+		UpdatedAt:        tools.ZeroIfNil(formatTimestamp(tools.PtrTo(time.Now()))),
+		Name:             name,
+		PresentationName: name,
+		Origin:           "uaa",
+		Links: UserLinks{
+			Self: Link{
+				HRef: buildURL(baseURL).appendPath(usersBase, name).build(),
+			},
+		},
+	}
 }
