09e1d9a40b22290f0241cba42d11ba9781329540
Konstantin Lapkov <k.lapkov@sap.com>
2025-03-19 13:18:24 +0200

    Mock user endpoints

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -11,6 +11,7 @@ import (
 
 const (
 	usersPath = "/v3/users"
+	userPath  = "/v3/users/{guid}"
 )
 
 type User struct {
@@ -23,6 +24,21 @@ func NewUser(apiBaseURL url.URL) User {
 	}
 }
 
+func (h User) create(req *http.Request) (*routing.Response, error) {
+	user := routing.URLParam(req, "guid")
+	return routing.NewResponse(http.StatusCreated).WithBody(presenter.ForUser(user, h.apiBaseURL)), nil
+}
+
+func (h User) get(req *http.Request) (*routing.Response, error) {
+	user := routing.URLParam(req, "guid")
+	return routing.NewResponse(http.StatusOK).WithBody(presenter.ForUser(user, h.apiBaseURL)), nil
+}
+
+func (h User) update(req *http.Request) (*routing.Response, error) {
+	user := routing.URLParam(req, "guid")
+	return routing.NewResponse(http.StatusCreated).WithBody(presenter.ForUser(user, h.apiBaseURL)), nil
+}
+
 func (h User) list(req *http.Request) (*routing.Response, error) {
 	usernames := req.URL.Query().Get("usernames")
 	users := []string{}
@@ -32,6 +48,10 @@ func (h User) list(req *http.Request) (*routing.Response, error) {
 	return routing.NewResponse(http.StatusOK).WithBody(presenter.ForList(presenter.ForUser, users, h.apiBaseURL, *req.URL)), nil
 }
 
+func (h User) delete(req *http.Request) (*routing.Response, error) {
+	return routing.NewResponse(http.StatusAccepted), nil
+}
+
 func (h User) UnauthenticatedRoutes() []routing.Route {
 	return nil
 }
@@ -39,5 +59,9 @@ func (h User) UnauthenticatedRoutes() []routing.Route {
 func (h User) AuthenticatedRoutes() []routing.Route {
 	return []routing.Route{
 		{Method: "GET", Pattern: usersPath, Handler: h.list},
+		{Method: "GET", Pattern: userPath, Handler: h.get},
+		{Method: "PATCH", Pattern: userPath, Handler: h.update},
+		{Method: "POST", Pattern: usersPath, Handler: h.create},
+		{Method: "DELETE", Pattern: userPath, Handler: h.delete},
 	}
 }
