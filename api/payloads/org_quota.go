ab4e20919898f59bd1f4f38687cfcdb3a7c6c9ea
Konstantin Lapkov <k.lapkov@sap.com>
2025-04-03 02:42:39 +0300

    More tweaks to quotas

--- api/payloads/org_quota.go
+++ api/payloads/org_quota.go
@@ -2,14 +2,22 @@ package payloads
 
 import (
 	"net/url"
+	"regexp"
 )
 
 type OrgQuotasList struct {
-	Names string
+	Names string `json:"names"`
 }
 
 func (l OrgQuotasList) SupportedKeys() []string {
-	return []string{"guids", "organization_guids", "names"}
+	return []string{"guids", "organization_guids", "names", "order_by"}
+}
+
+func (d *OrgQuotasList) IgnoredKeys() []*regexp.Regexp {
+	return []*regexp.Regexp{
+		regexp.MustCompile("page"),
+		regexp.MustCompile("per_page"),
+	}
 }
 
 func (l *OrgQuotasList) DecodeFromURLValues(values url.Values) error {
